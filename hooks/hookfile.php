ecd07b189eb26a7e79d529f23678a353d8687062
Domenic Del Nano <ddelnano@gmail.com>
2015-07-18 00:45:59 -0400

    Fixed failing unit test due to undefined variable

--- hooks/hookfile.php
+++ hooks/hookfile.php
@@ -2,42 +2,42 @@
 
 use Dredd\Hooks;
 
-Hooks::before('/message > GET', function(&$transaction) use ($key) {
+Hooks::before('/message > GET', function(&$transaction)  {
 
     echo "before hook handled";
 });
 
-Hooks::after('/message > GET', function(&$transaction) use ($key) {
+Hooks::after('/message > GET', function(&$transaction)  {
 
     echo "after hook handled";
 });
 
-Hooks::beforeValidation('/message > GET', function(&$transaction) use ($key) {
+Hooks::beforeValidation('/message > GET', function(&$transaction)  {
 
     echo 'before validation hook handled';
 });
 
-Hooks::beforeAll(function(&$transaction) use ($key) {
+Hooks::beforeAll(function(&$transaction)  {
 
     echo 'before all hook handled';
 });
 
-Hooks::afterAll(function(&$transaction) use ($key) {
+Hooks::afterAll(function(&$transaction)  {
 
     echo 'after all hook handled';
 });
 
-Hooks::beforeEach(function(&$transaction) use ($key) {
+Hooks::beforeEach(function(&$transaction)  {
 
     echo 'before each hook handled';
 });
 
-Hooks::beforeEachValidation(function(&$transaction) use ($key) {
+Hooks::beforeEachValidation(function(&$transaction)  {
 
     echo 'before each validation hook handled';
 });
 
-Hooks::afterEach(function(&$transaction) use ($key) {
+Hooks::afterEach(function(&$transaction)  {
 
     echo 'after each hook handled';
 });
