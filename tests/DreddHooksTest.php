e7ff8699c3bff1d01d431e6ff42bfac511aa01e0
Domenic Del Nano <ddelnano@gmail.com>
2015-07-19 22:02:57 -0400

    Added tearDown method to reset Hooks static variables and added test for adding hooks for same transaction

--- tests/DreddHooksTest.php
+++ tests/DreddHooksTest.php
@@ -1,10 +1,23 @@
 <?php
 
-
 use Dredd\Hooks;
 
 class DreddHooksTest extends PHPUnit_Framework_TestCase
 {
+    public function tearDown()
+    {
+        Hooks::$beforeAllHooks = [];
+        Hooks::$afterAllHooks = [];
+
+        Hooks::$beforeHooks = [];
+        Hooks::$beforeValidationHooks = [];
+        Hooks::$afterHooks = [];
+
+        Hooks::$beforeEachHooks = [];
+        Hooks::$beforeEachValidationHooks = [];
+        Hooks::$afterEachHooks = [];
+    }
+
     /**
      * @test
      */
@@ -13,5 +26,24 @@ class DreddHooksTest extends PHPUnit_Framework_TestCase
         Hooks::loadHooks([__DIR__ . "/../hooks/hookfile.php"]);
 
         $this->assertCount(1, Hooks::$beforeHooks);
+    }
+
+    /**
+     * @test
+     */
+
+    public function it_can_load_multiple_before_hooks_for_same_transaction_name()
+    {
+        $name = 'transaction';
+
+        Hooks::before($name, function(&$transaction) {
+
+        });
+
+        Hooks::before($name, function(&$transaction) {
+
+        });
+
+        $this->assertCount(2, Hooks::$beforeHooks[$name]);
     }
 }
