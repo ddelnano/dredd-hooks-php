939c4979f401e8f4b5958dbdadbb79f99ef9da40
Benedikt Franke <benedikt.franke@zoho.com>
2018-02-04 13:27:59 +0100

    Refresh migration and seed before tests
    This is a workaround if transactions do not work, due to the fact that the hooks and Laravel run in a different PHP Execution context.

--- examples/laravel/tests/dredd/hooks/hookfile.php
+++ examples/laravel/tests/dredd/hooks/hookfile.php
@@ -1,13 +1,16 @@
 <?php
 
 use Dredd\Hooks;
-use Illuminate\Support\Facades\Artisan;
 
 require __DIR__ . '/../../../vendor/autoload.php';
 
 $app = require __DIR__ . '/../../../bootstrap/app.php';
 
 $app->make(\Illuminate\Contracts\Console\Kernel::class)->bootstrap();
+
+Hooks::beforeAll(function (&$transaction) {
+    Illuminate\Support\Facades\Artisan::call('migrate:refresh', ['--seed' => true]);
+});
 
 Hooks::beforeEach(function (&$transaction) use ($app) {
     $app->make('db')->beginTransaction();
